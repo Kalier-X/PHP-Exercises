c5a9c730bd8d5f4c8837017ffbaf97ebb6c338b2
Kalier-X <34463785+Kalier-X@users.noreply.github.com>
2018-03-23 10:04:50 +0800

    Add files via upload

---
+++ Hello Word.php
@@ -0,0 +1,7 @@
+<?PHP
+echo "This is my first attempt to use github , althoung i am Chinese , but i try to use English to improve my English ! <br>";
+echo "这是我第一次去尝试使用github，虽然我是中国人，但是我尽量使用英语，提高自己英语水平。";
+
+
+
+?>
